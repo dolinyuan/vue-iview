6a6860d2a58c7322e6b8e879316be87694d9a0b8
Yuan chunxia <245947540@qq.com>
2019-01-10 00:30:23 +0800

    测试switch开关

--- src/views/formitem/FormItemRepeatName.vue
+++ src/views/formitem/FormItemRepeatName.vue
@@ -1,10 +1,15 @@
 <template>
+    <div>
+    <p>https://www.cnblogs.com/lvdabao/p/es6-promise-1.html</p>
+    <p>https://segmentfault.com/a/1190000010345031</p>
     <Form ref="formDynamic" :model="formDynamic" :label-width="80" style="width: 300px">
         <ul v-if="formDynamic.items">
             <li v-for="item in formDynamic.items">
                 {{item}}
             </li>
         </ul>
+        <div @click="switchClick">Switch</div>
+        <div v-if="!isShow">
         <FormItem
                 v-for="(item, index) in formDynamic.items"
                 v-if="item.status"
@@ -29,11 +34,14 @@
                 </Col>
             </Row>
         </FormItem>
+        </div>
+        <input v-if="isShow" type="textarea" v-model="formDynamic.blukValues"></input>
         <FormItem>
             <Button type="primary" @click="handleSubmit('formDynamic')">Submit</Button>
             <Button @click="handleReset('formDynamic')" style="margin-left: 8px">Reset</Button>
         </FormItem>
     </Form>
+    </div>
 </template>
 <script>
 export default {
@@ -57,7 +65,9 @@ export default {
 
             return {
                 index: 1,
+                isShow:false,
                 formDynamic: {
+                    blukValues:"",
                     items: [
                         {
                             value: '',
@@ -68,8 +78,6 @@ export default {
                 },
                 funRules:{validator:validateCheckRepeat, trigger: 'change'},
                 /*funRules:{required: true, message: '变量不能为空', trigger: 'blur'}*/
-
-
             }
         },
         watch:{
@@ -78,9 +86,30 @@ export default {
                     this.$refs['formDynamic'].validate()
                 },
                 deep:true
+            },
+            'formDynamic.items':{
+                handler(val){
+                },
+                deep:true
             }
         },
         methods: {
+            switchClick(){
+                /*this.isShow = !this.isShow;
+                this.$refs['formDynamic'].validate().then((result)=>{
+                    if(this.isShow){
+                        console.log(this.isShow)
+                        this._renderBlukValues(this.formDynamic.items)
+                    }
+                })*/
+
+            },
+            _renderBlukValues(values){
+                this.formDynamic.blukValues = ""
+                values.forEach((item,index)=>{
+                    this.formDynamic.blukValues += item.value;
+                })
+            },
             check(data,index){
                 var currentValue = data.value
                 var itemValueArray = [];
